d2c5d70d3a69fb72185168822bf3646fcd221340
USER-HUB123 <68796019+USER-HUB123@users.noreply.github.com>
2022-09-24 07:56:58 -0400

    Add files via upload

--- main/GameUI/__init__.py
+++ main/GameUI/__init__.py
@@ -1,10 +1,20 @@
+from threading import Event
 from PIL import Image
-#from . import Card
-import Card
+from . import Card
 
 def makeCharacterCard(character:str):
     return Card.CharacterCard(character)
 
-if __name__ == "__main__":
-    card = makeCharacterCard("Julius Caesar")
-    print(card.image)
+#make game object for UI
+class Game:
+    def __init__(self):
+        pass
+
+    #on receiving playTurn, UI starts internal functions for preturn, drawphase, ...
+    def playTurn(self):
+        pass
+
+    #create function to draw cards (sending a request to the server) at the beginning of each turn
+    #convert to Card object
+
+    #if card number in hand exceeds 4, ask for discard phase
